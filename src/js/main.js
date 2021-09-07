f824a28835d5c2c6596255cf5eb331f902a02007
Olena Honcharuk <gasmin07@gmail.com>
2021-09-07 23:44:03 +0300

    refactor: add rest param to class MenuCard and default param to render

--- src/js/main.js
+++ src/js/main.js
@@ -136,12 +136,13 @@ window.addEventListener("DOMContentLoaded", () => {
 
   //classes for cards
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes; // array, not a string
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
       this.changeToUah();
@@ -152,8 +153,15 @@ window.addEventListener("DOMContentLoaded", () => {
 
     render() {
       const element = document.createElement("div");
+      if (this.classes.length === 0) {
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
+
       element.innerHTML = `
-        <div class="menu__item">
+        
           <img src=${this.src} alt=${this.alt} />
           <h3 class="menu__item-subtitle">${this.title}</h3>
           <div class="menu__item-descr">
@@ -166,8 +174,7 @@ window.addEventListener("DOMContentLoaded", () => {
               <span>${this.price}</span> грн/день
             </div>
           </div>
-        </div>
-        `;
+          `;
       this.parent.append(element);
     }
   }
@@ -178,7 +185,8 @@ window.addEventListener("DOMContentLoaded", () => {
     'Меню "Фитнес"',
     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
     9,
-    ".menu .container"
+    ".menu .container",
+    "menu__item"
   ).render();
   new MenuCard(
     "img/tabs/post.jpg",
@@ -186,7 +194,8 @@ window.addEventListener("DOMContentLoaded", () => {
     'Меню "Постное"',
     "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
     14,
-    ".menu .container"
+    ".menu .container",
+    "menu__item"
   ).render();
 
   new MenuCard(
@@ -195,6 +204,7 @@ window.addEventListener("DOMContentLoaded", () => {
     "Меню “Премиум”",
     "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
     21,
-    ".menu .container"
+    ".menu .container",
+    "menu__item"
   ).render();
 });
